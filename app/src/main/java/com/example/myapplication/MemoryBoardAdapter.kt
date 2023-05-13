1481ad7f942d08f1da74cbbf817517aa2fae9375
Faithful Austin - Ilagha <missfair42@gmail.com>
2023-05-13 22:04:58 +0100

    Adjusted the space below

--- app/src/main/java/com/example/myapplication/MemoryBoardAdapter.kt
+++ app/src/main/java/com/example/myapplication/MemoryBoardAdapter.kt
@@ -4,14 +4,24 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
+import kotlin.math.min
 
 class MemoryBoardAdapter(private val context: Context, private val numPieces: Int) :
     RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val cardWidth : Int = parent.width / 2
+        val cardHeight : Int = parent.height / 2
+
+        val cardSideLength= min(cardWidth,cardHeight)
         val view = LayoutInflater.from(context).inflate(R.layout.memory_card, parent, false)
+        val layoutParams = view.findViewById<CardView>(R.id.cardView).layoutParams
+        layoutParams.width = cardSideLength
+        layoutParams.height = cardSideLength
+
         return ViewHolder(view)
     }
 

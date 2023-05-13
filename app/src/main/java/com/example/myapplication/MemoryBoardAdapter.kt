ea84ef9842a5317de7b125d2af8c72e2699ed4b8
Faithful Austin - Ilagha <missfair42@gmail.com>
2023-05-13 21:49:40 +0100

    MemoryCard added

--- app/src/main/java/com/example/myapplication/MemoryBoardAdapter.kt
+++ app/src/main/java/com/example/myapplication/MemoryBoardAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -8,17 +9,28 @@ import androidx.recyclerview.widget.RecyclerView
 class MemoryBoardAdapter(private val context: Context, private val numPieces: Int) :
     RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-        inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(context).inflate(R.layout.memory_card, parent, false)
+        return ViewHolder(view)
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount() = numPieces
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+       holder.bind(position)
     }
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        
+    }
+
 }
+
+private fun RecyclerView.ViewHolder.bind(position: Int) {
+
+}
+
+
+
+
+
